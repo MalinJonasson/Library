fe6b699edac19f612bda6b7216fdd6cb20719e98
MalinJonasson <147442348+MalinJonasson@users.noreply.github.com>
2024-11-18 16:40:37 +0100

    Rearenged files, added API, tests, controllers, fakedatabase

---
+++ API/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Application;
+using Domain;
+using Infrastructure.DataBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : Controller
+    {
+        //CRUD GET UPDATE/PUT/PATCH POST DELETE
+
+        [HttpPost]
+        public Book AddBook()
+        {
+            BookMethods applicationLayerBookMethods = new BookMethods();
+            return applicationLayerBookMethods.AddNewBook();
+        }
+
+        [HttpGet]
+        public Book GetAllBooks()
+        {
+            BookMethods applicationLayerBookMethods = new BookMethods();
+            return applicationLayerBookMethods.GetAllBooks();
+        }
+
+        [HttpPut]
+        public Book UpdateABook()
+        {
+            BookMethods applicationLayerBookMethods = new BookMethods();
+            return applicationLayerBookMethods.UpdateABook();
+        }
+
+        [HttpDelete]
+        public Book DeleteSBook()
+        {
+            BookMethods applicationLayerBookMethods = new BookMethods();
+            return applicationLayerBookMethods.DeleteBook();
+        }
+    }
+}
---
+++ Application/BookMethods.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using Infrastructure.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class BookMethods
+    {
+        private readonly FakeDatabase _fakeDatabase;
+
+        public BookMethods(){}
+        public BookMethods(FakeDatabase fakeDatabase)
+        {
+            _fakeDatabase = fakeDatabase;
+        }
+
+        //Vallidering/felhantering
+        public Book AddNewBook()
+        {
+            //How to process data in application
+
+            Book newBookToAdd = new Book(1, "Book1", "AboutBook1");
+
+            //Seperations of consern, returnerar vad infrastrukturen gör
+            return _fakeDatabase.AddNewBook(newBookToAdd);
+        }
+
+        public Book DeleteBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book UpdateABook()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
