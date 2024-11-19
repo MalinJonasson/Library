385de5999b2d84b115f3cf1dacd6793270d835d6
MalinJonasson <147442348+MalinJonasson@users.noreply.github.com>
2024-11-19 10:00:17 +0100

    Added Author

---
+++ Domain/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Author(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
--- Infrastructure/DataBase/FakeDatabase.cs
+++ Infrastructure/DataBase/FakeDatabase.cs
@@ -5,11 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.DataBase
+namespace Infrastructure.Database
 {
     public class FakeDatabase
     {
         public List<Book> Books { get { return allBooksFromDB;  } set { allBooksFromDB = value;  } }
+        public List<Author> Author { get { return allAuthorsFromDB; } set { allAuthorsFromDB = value; } }
 
         List<Book> allBooksFromDB = new List<Book>()
         {
@@ -18,6 +19,16 @@ namespace Infrastructure.DataBase
             new Book (3, "Book3", "AboutBook3"),
             new Book (4, "Book4", "AboutBook4"),
             new Book (5, "Book5", "AboutBook5"),
+        };
+
+
+        List<Author> allAuthorsFromDB = new List<Author>()
+        {
+            new Author (1, "Author1"),
+            new Author (2, "Author2"),
+            new Author (3, "Author3"),
+            new Author (4, "Author4"),
+            new Author (5, "Author5"),
         };
 
         // My CRUD Methods
@@ -31,6 +42,17 @@ namespace Infrastructure.DataBase
         public void DeleteBook(Book book)
         {
             allBooksFromDB.Remove(book);
+        }
+
+        public Author AddNewAuhtor(Author author)
+        {
+            allAuthorsFromDB.Add(author);
+            return author;
+        }
+
+        public void DeleteAuthor(Author author)
+        {
+            allAuthorsFromDB.Remove(author);
         }
     }
 }
